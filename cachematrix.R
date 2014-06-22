bae8655e1c07199753eccf6cf2bc0125cfb99715
Xiao Wang <shwangxiao@gmail.com>
2014-06-22 17:35:09 -0400

    matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
@@ -12,4 +22,31 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
+#Testing
+
+k <- makeCacheMatrix()
+
+#Construct a matrix
+a = matrix(c(1,2,3,4),nrow=2,ncol=2)
+
+#initialize matrix of k
+k$set(a)
+#test to display
+k$get()
+
+#First time call
+cacheSolve(k)
+
+#2nd time call, shall be able to display "getting cached data"
+cacheSolve(k)
